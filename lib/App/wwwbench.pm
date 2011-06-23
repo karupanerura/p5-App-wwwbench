32d938ccafbec7e2af6e4f66c2f4d370b396de64
Kenta Sato <karupa@cpan.org>
2011-06-23 22:46:49 +0900

    initial commit

---
+++ lib/App/wwwbench.pm
@@ -0,0 +1,32 @@
+package App::wwwbench;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::wwwbench -
+
+=head1 SYNOPSIS
+
+  use App::wwwbench;
+
+=head1 DESCRIPTION
+
+App::wwwbench is
+
+=head1 AUTHOR
+
+Kenta Sato E<lt>karupa@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
